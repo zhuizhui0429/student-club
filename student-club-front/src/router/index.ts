5ed438a9584daa521d6854a419e92148d0408a21
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-25 17:55:22 +0800

    feat: 初始化路由

---
+++ student-club-front/src/router/index.ts
@@ -0,0 +1,50 @@
+import { RouterOptions, createRouter, createWebHistory } from 'vue-router'
+const Login = () => import('../pages/login.vue')
+const Register = () => import('../pages/register.vue')
+const HomePage = () => import('@/pages/homePage.vue')
+const ClubSquare = () => import('@/pages/clubSquare.vue')
+const personalCenter = () => import(('@/pages/personalCenter.vue'))
+const MyMessage = () => import('@/pages/myMessage.vue')
+
+
+const routes: RouterOptions['routes'] = [
+    {
+        path: '/',
+        component: HomePage,
+        children: [
+            {
+                path: 'clubSquare',
+                component: ClubSquare
+            },
+            {
+                path: 'personalCenter',
+                component: personalCenter
+            },
+            {
+                path: 'myMessage',
+                component: MyMessage
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/register',
+        component: Register
+    }
+]
+
+
+export const router = createRouter({
+    routes,
+    history: createWebHistory('')
+})
+
+// router.beforeEach((to, from) => {
+//     console.log('to', to)
+//     console.log('from', from)
+// })
+
+
