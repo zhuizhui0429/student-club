08a4bc75c67f13a1eb06bf8783b83f318093b06e
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-27 11:24:49 +0800

    feat: 封装统一loading组件

---
+++ student-club-front/src/components/loading.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    loading: {
+      type: Boolean,
+      default: true,
+    },
+    initialHeight: {
+      type: String,
+      default: "100%",
+    },
+    size: {
+      type: String,
+      validator(val) {
+        return ["small", "large", "default"].includes(val as string);
+      },
+      default: "default",
+    },
+    backgroundColor: {
+      type: String,
+      default: "#dcebe4",
+    },
+  },
+  setup() {},
+});
+</script>
+
+<template>
+  <div
+    class="loading_container"
+    v-show="loading"
+    :style="`height:${initialHeight};background-color:${backgroundColor}`"
+  >
+    <a-spin tip="数据加载中..." :size="size" :spinning="loading"></a-spin>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.loading_container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  z-index: 999;
+  top: 0;
+  left: 0;
+}
+</style>
