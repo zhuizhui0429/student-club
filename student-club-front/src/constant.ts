f5dac3b8891af3bc63814586ad0a24c07bf5a40d
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-27 11:25:30 +0800

    feat: 常量统一处理

---
+++ student-club-front/src/constant.ts
@@ -0,0 +1,5 @@
+export const colleges = ['计网院', '法学院', '外语院'] as const
+
+export const grades = ['大一', '大二', '大三'] as const
+
+export const defaultAvatar = 'https://t7.baidu.com/it/u=3000713993,473068593&fm=218&app=126&size=f242,150&n=0&f=JPEG&fmt=auto?s=1C0A3774475041645C2B38F90200D03C&sec=1682701200&t=7527fbed242b4c13f31d2d9929d2396a'
