6ce6e3635cfbf46b3e280112ec409e394272dd74
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-27 11:46:53 +0800

    feat: 单独封装头像上传组件

---
+++ student-club-front/src/components/avatarUpload.vue
@@ -0,0 +1,79 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { UploadOutlined } from "@ant-design/icons-vue";
+import { defaultAvatar } from "@/constant";
+
+function getBase64(img: Blob) {
+  return new Promise<string>((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => resolve(reader.result as string));
+    reader.readAsDataURL(img);
+  });
+}
+export default defineComponent({
+  props: {
+    initialAvatar: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["update:avatarFile"],
+  setup(props, { emit }) {
+    const { initialAvatar } = props;
+    const selectedAvatar = ref<string>(initialAvatar);
+
+    const beforeUpload = async (file: File) => {
+      const temptUrl = await getBase64(file);
+      selectedAvatar.value = temptUrl;
+      emit("update:avatarFile", file);
+      return false;
+    };
+
+    return {
+      selectedAvatar,
+      beforeUpload,
+      defaultAvatar,
+    };
+  },
+  components: {
+    UploadOutlined,
+  },
+});
+</script>
+
+<template>
+  <div class="avatar_upload_container">
+    <a-upload :showUploadList="false" :before-upload="beforeUpload">
+      <a-button>
+        <upload-outlined></upload-outlined>
+        点击上传头像
+      </a-button>
+    </a-upload>
+    <a-image
+      class="avatar"
+      :width="100"
+      :height="100"
+      :src="selectedAvatar"
+      :fallback="defaultAvatar"
+      alt=""
+    />
+  </div>
+</template>
+
+<style lang="less">
+.avatar_upload_container {
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .ant-image {
+    margin-left: 20px;
+    .ant-image-img {
+      width: 100px;
+      height: 100px;
+      object-fit: cover;
+      border-radius: 10px;
+    }
+  }
+}
+</style>
