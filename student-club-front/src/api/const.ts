8a051f2d9e51f6b94aa81ce5b67d8a3548af431d
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-04-30 21:51:42 +0800

    feat: 统一接口成功或者失败的message提示

---
+++ student-club-front/src/api/const.ts
@@ -0,0 +1,7 @@
+export const needTipRequest: string[] = [
+    '/club/create',
+    '/activity/create',
+    '/activity/update',
+    '/user/updateInfo',
+    '/user/login'
+]
--- student-club-front/src/api/index.ts
+++ student-club-front/src/api/index.ts
@@ -1,15 +1,31 @@
 import type { LoginPayload, ResponseFormatter, Club, UserInfo } from './interface'
 import axios from 'axios'
+import { needTipRequest } from './const'
+import { message } from 'ant-design-vue'
 
 export const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 3000
 })
 
+
+
+axiosInstance.interceptors.response.use((res) => {
+    const requestUrl = res.request.responseURL as string
+    const successInfo = res.data.message
+    if (needTipRequest.find(target => requestUrl.includes(target))) {
+        message.success(successInfo, 2)
+    }
+    return res
+}, err => {
+    console.log('请求失败', err)
+})
+
 export * from './interface'
 export * from './message'
 export * from './manager'
 export * from './admin'
+export * from './student'
 
 export function login(payload: LoginPayload) {
     return axiosInstance.post<ResponseFormatter<UserInfo>>('/user/login', {
