d7ab306364466a304145dd3caa4fda4a96676976
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-05-02 16:07:47 +0800

    fix: 优化邮箱内容

--- student-club-backend/src/email.ts
+++ student-club-backend/src/email.ts
@@ -19,18 +19,25 @@ interface SendEmailPayloadType {
 }
 
 const subjectMap: Record<EmailType, string> = {
-    welcomeJoin: '欢迎加入俱乐部',
-    publishActivity: '你加入的俱乐部发布新活动了',
-    updateActivity: '你加入的俱乐部最近更新了活动'
+    welcomeJoin: 'Welcome to the club',
+    publishActivity: 'Your club has released a new activity',
+    updateActivity: 'Your club has recently updated.'
+}
+
+const contentMap: Record<EmailType, string> = {
+    welcomeJoin:
+        `Welcome to the student club! We are thrilled to have you join us. We hope you will enjoy your time with us and make many new friends. If you have any questions or concerns, please don’t hesitate to ask. We are here to help you in any way we can.`,
+    publishActivity: `“Hello everyone! We are excited to announce our new activity. We hope you can join us for this fun event. Please RSVP  so we can plan accordingly. If you have any questions or concerns, please don’t hesitate to ask. We look forward to seeing you there!”`,
+    updateActivity: `“Hello everyone! We have an exciting update for you. We have added new activities to our schedule. Please check our website for more information. We hope you can join us for these fun events. If you have any questions or concerns, please don’t hesitate to ask. We look forward to seeing you there!”`
 }
 
 export const sendEmail = (payload: SendEmailPayloadType) => {
     const { clubName, type, to, activityName } = payload
     transporter.sendMail({
-        from: `"${clubName} 俱乐部"<2301766839@qq.com>`, // sender address
+        from: `"${clubName}"<2301766839@qq.com>`, // sender address
         to, // list of receivers
         subject: subjectMap[type], // Subject line
-        text: "这是邮箱内容", // plain text body
+        text: contentMap[type], // plain text body
     }).then(res => {
         console.log('sucesss', res)
     }).catch(err => {
